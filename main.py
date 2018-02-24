516bed324860d0da3d9b0b11da171d1ddac7fb89
Brandon <brandonskerritt51@gmail.com>
2018-02-24 16:57:44 +0000

    This is an automated script to quickly update this Git repo. No important features here.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-"""from twilio.rest import Client
+from twilio.rest import Client
 from twilio.twiml.messaging_response import Body, Message, Redirect, MessagingResponse
 
 
 
 account = "ACf6ddb664419f225eb09901d32dbfc5c9"
 token = "345335f019dfac112d93292888a4ca5f"
-client = Client(account, token)"""
+client = Client(account, token)
 
 #message = client.messages.create(to="+447534003420", from_="+441618502075", body="Hello there!")
 
@@ -14,16 +14,34 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
-@app.route("/inbound", methods=['GET', 'POST'])
-def sms_reply():
+client = Client(account, token)
+
+def send_message(message, send_to_num, from_num="+441618502075"):
+    """ function to send "message" to "send_to_num" from default num, "from_num" """
+    client.api.account.messages.create(
+    to = send_to_num,
+    from_ = from_num,
+    body=message)
+    # internal confirmation
+    print("Message {} sent to {} from {}",format(message, from_num, send_to_num))
+
+def sms_get_body_define(message)
+    """ simple function to determine what was in the body """
+
+    return None
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_get_body():
     """Respond to incoming calls with a simple text message."""
     # Start our TwiML response
     resp = MessagingResponse()
 
-    # Add a message
-    resp.message("The Robots are coming! Head for the hills!")
+    body = request.values.get('Body', None)
+    from = request.values.get("From", None)
+    resp.message("Hello, you said {}".format(body))
 
     return str(resp)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
